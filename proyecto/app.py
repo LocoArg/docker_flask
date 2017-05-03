bbecbd7b7bcf9cdebea00e4d585ce5b9e3128017
Emiliano Chaves <chaves.emiliano@gmail.com>
2017-05-03 14:59:05 -0300

    Finalizado, primer video

--- proyecto/app.py
+++ proyecto/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from data import Articles
 
 app = Flask(__name__)
+
+Articles = Articles()
 
 @app.route('/')
 def index():
@@ -10,7 +13,10 @@ def about():
     return render_template('about.html')
 @app.route('/articles')
 def articles():
-    return render_template('articles.html')
+    return render_template('articles.html', articles=Articles)
+@app.route('/article/<string:id>/')
+def article(id):
+    return render_template('article.html', id=id)
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=5000)
