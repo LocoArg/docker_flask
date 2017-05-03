aa2d1efc77e0f5ddb13bddbc3b8e43619890fbf6
Emiliano Chaves <chaves.emiliano@gmail.com>
2017-05-03 12:52:37 -0300

    Se agregaron nuevas paginas

--- proyecto/app.py
+++ proyecto/app.py
@@ -5,6 +5,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('home.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
+@app.route('/articles')
+def articles():
+    return render_template('articles.html')
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=5000)
